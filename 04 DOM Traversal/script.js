ed37474ef0065460570ae6f23813a6d982ada8c2
Ricky Harefa <114171685+ricky-hrf@users.noreply.github.com>
2025-07-14 21:38:58 +0700

    Latihan DOM Traversal

---
+++ 04 DOM Traversal/script.js
@@ -0,0 +1,22 @@
+// // cara biasa
+// const closeButtons = document.querySelector(".close")
+// const card = document.querySelector(".card")
+
+// closeButtons.addEventListener("click", function () {
+//   card.style.display = "none";
+// });
+
+// cara dengan DOM Traversal
+const closeButtons = document.querySelectorAll(".close");
+
+// for (let i = 0; i < closeButtons.length; i++){
+//   closeButtons[i].addEventListener("click", function (event) {
+//     event.target.parentElement.style.display = "none";
+//   });
+// }
+
+closeButtons.forEach(function (closeButton) {
+  closeButton.addEventListener("click", function (event) {
+    event.target.parentElement.style.display = "none";
+  });
+});
