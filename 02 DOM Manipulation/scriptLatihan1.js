9a10982cf394daf1290320a0891adceab91890bf
Ricky Harefa <114171685+ricky-hrf@users.noreply.github.com>
2025-08-04 22:08:31 +0700

    clean code

--- 02 DOM Manipulation/scriptLatihan1.js
+++ 02 DOM Manipulation/scriptLatihan1.js
@@ -1,31 +1,86 @@
-let container = document.querySelector(".container");
-let produk = document.querySelector(".produk");
-let card = document.querySelector(".card");
+let products = [
+  {
+    "nama": "barang1",
+    "harga": 20000,
+    "stok": 12,
+    "desc":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at quam nec erat facilisis tincidunt. Sed non ligula a enim facilisis aliquet."
+  },
+  {
+    "nama": "barang2",
+    "harga": 30000,
+    "stok":13,
+    "desc":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at quam nec erat facilisis tincidunt. Sed non ligula a enim facilisis aliquet."
+  },
+  {
+    "nama": "barang3",
+    "harga": 30000,
+    "stok":14,
+    "desc":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at quam nec erat facilisis tincidunt. Sed non ligula a enim facilisis aliquet."
+  },
+  {
+    "nama": "Semangka",
+    "harga": 50000,
+    "stok":10,
+    "desc":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at quam nec erat facilisis tincidunt. Sed non ligula a enim facilisis aliquet."
+  }
+]
 
-// manipulasi node
-// 1. document.createElement
-let newCard = document.createElement("div");
-newCard.classList.add("card");
-newCard.innerHTML = `
-  <div class="card-header">
-    <span class="nama">Produk 1</span>
-    <img src="#" alt="#" class="card-img">
-  </div>
-  <div class="card-body">
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at quam nec erat facilisis tincidunt. Sed non ligula a enim facilisis aliquet.</p>
-  </div>
-  <div class="card-footer">
-    <form action="">
-      <button type="submit">Buy</button>
-      <button type="button">Add to Cart</button>
-    </form>
-  </div>
-`;
+let produk = document.querySelector(".produk")
+products.forEach(brg => {
+  let card = document.createElement("div");
+  card.className = "card";
+  produk.appendChild(card);
+  let cardHeader = document.createElement("div")
+  cardHeader.className = "card-header";
+  card.appendChild(cardHeader);
+  let image = document.createElement("img");
+  image.src = "#";
+  image.alt = "#";
+  image.className = "card-img";
+  cardHeader.appendChild(image);
+  let cardBody = document.createElement("div");
+  cardBody.className = "card-body";
+  card.appendChild(cardBody);
+  let info = document.createElement("div")
+  info.className = "info";
+  cardBody.appendChild(info);
+  let nama = document.createElement("span")
+  nama.innerHTML = brg["nama"]
+  info.appendChild(nama);
 
-// 2. node.appendChild()
-container.appendChild(newCard);
-// 3. node.insertBefore()
-let newProduk = document.createElement("div");
-newProduk.classList.add("produk");
-newProduk.appendChild(newCard);
-produk.insertBefore(newProduk, card);
+  let harga = document.createElement("span")
+  harga.innerHTML = brg["harga"].toLocaleString("id-ID");
+  info.appendChild(harga);
+
+  let info2 = document.createElement("div");
+  info2.className = "info2";
+  cardBody.appendChild(info2);
+
+  let stok = document.createElement("p");
+  stok.innerHTML = brg['stok'];
+  info2.appendChild(stok);
+
+  let desc = document.createElement("div");
+  desc.className = "desc";
+  cardBody.appendChild(desc);
+
+  let description = document.createElement("p");
+  description.className = "description";
+  description.innerHTML = brg["desc"]
+  desc.appendChild(description);
+
+  let cardFooter = document.createElement("div");
+  cardFooter.className = "card-footer";
+  card.appendChild(cardFooter);
+
+  let bBuy = document.createElement("button");
+  bBuy.type = "submit";
+  bBuy.innerHTML = "Buy";
+  cardFooter.appendChild(bBuy);
+
+  let bAdd = document.createElement("button");
+  bAdd.type = "button";
+  bAdd.innerHTML = "Add to Cart";
+  cardFooter.appendChild(bAdd);
+}
+)
